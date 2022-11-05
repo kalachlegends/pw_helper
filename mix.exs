c17810159285d90952bd62c2e0ee3112bed7eec1
Artem Kalachev <artem.kalachev.2002@gmail.com>
2022-11-05 09:12:52 +0600

    to elixir 1.12

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule PwHelper.MixProject do
     [
       app: :pw_helper,
       version: "0.1.0",
-      elixir: "~> 1.13",
+      elixir: "~> 1.12.2",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
