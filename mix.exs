40833fcdbe52b88da4b057a6f6b0668cab840407
artem <artem.kalachev.2002@gmail.com>
2023-03-02 15:59:26 +0600

    fix mix

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule PwHelper.MixProject do
     [
       app: :pw_helper,
       version: "0.1.0",
-      elixir: "~> 1.13.2",
+      elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
